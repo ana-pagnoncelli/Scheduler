e57c859b9cbcf18ee4ee4823b8f5638e61084075
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-02-28 00:51:22 -0300

    Fix routes redirect

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,17 +3,13 @@ import { Navigate, useRoutes } from "react-router-dom";
 import { Home, NotFound, Expenses } from "./pages";
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: "/",
-      element: <Home />,
-      children: [
-        { path: "expenses", element: <Expenses /> },
-        // { path: "/", element: <Navigate to='/introducao' /> },
-        { path: "404", element: <NotFound /> },
-        { path: "*", element: <Navigate to='/404' /> }
-      ]
-    },
-    { path: "*", element: <Navigate to='/404' replace /> }
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "expenses", element: <Expenses /> },
+    { path: "/", element: <Navigate to='/expenses' /> },
+    { path: "404", element: <NotFound /> },
+    { path: "*", element: <Navigate to='/404' /> }
   ]);
+
+  return routes;
 }
