ac050eaa858035656415f4476aa75c306b23c867
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-03-03 18:28:55 -0300

    Creates cypress tests for Login Page

--- cypress/e2e/Drawer.cy.tsx
+++ cypress/e2e/Drawer.cy.tsx
@@ -1,12 +1,10 @@
-describe("Redirects when clicking on items on the drawer", () => {
+describe("Redirects when clicking on the scheduler item on the drawer", () => {
   it("should redirect to scheduler page when clicking in scheduler item", () => {
     cy.visit("http://localhost:3000");
 
     cy.get(`[aria-label="open drawer"]`).click();
     cy.get('[data-cy="Schedule Class"]').click();
 
-    // Should be on a new URL which
-    // includes '/commands/actions'
     cy.url().should("include", "/schedule-class");
   });
 });
---
+++ cypress/e2e/Login.cy.tsx
@@ -0,0 +1,37 @@
+describe("Redirect to login page", () => {
+  it("should redirect to login page when clicking in login item", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get(`[aria-label="open drawer"]`).click();
+    cy.get('[data-cy="Login"]').click();
+
+    cy.url().should("include", "/login");
+  });
+});
+
+describe("User registration", () => {
+  it("should register the user successfully when the data was filled", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.contains("Name").type("test name");
+    cy.contains("Email").type("test email");
+    cy.contains("Password").type("test password");
+
+    cy.contains("button", "Submit").click();
+
+    cy.contains("User test name successfully registered!!");
+  });
+
+  it("should fail to register the user when there is missing data", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.contains("Email").type("test email");
+    cy.contains("Password").type("test password");
+
+    cy.contains("button", "Submit").click();
+
+    cy.contains("Please enter all the fields");
+  });
+});
+
+export {};
