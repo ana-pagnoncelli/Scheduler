65fee5617b142a9deaff0a67ebd4f5ed903ebfa0
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-03-03 17:14:38 -0300

    Create unit test case for succesfully filling the form

--- src/pages/login/Login.test.tsx
+++ src/pages/login/Login.test.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Login } from "./Login";
 
-test("renders scheduler page", () => {
-  render(<Login />);
-  const linkElement = screen.getByText("User Registration");
-  expect(linkElement).toBeInTheDocument();
+describe("Login", () => {
+  beforeEach(() => {
+    render(<Login />);
+  });
+  it("renders Login page", () => {
+    const linkElement = screen.getByText("User Registration");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  describe("user fill the form to user registration", () => {
+    it("should show success message if all the data was filled", () => {
+      const inputName = screen.getByLabelText("Name");
+      fireEvent.change(inputName, { target: { value: "test name" } });
+
+      const inputEmail = screen.getByLabelText("Email");
+      fireEvent.change(inputEmail, { target: { value: "test email" } });
+
+      const inputPassword = screen.getByLabelText("Password");
+      fireEvent.change(inputPassword, { target: { value: "test password" } });
+
+      const submitButton = screen.getByRole("button", { name: "Submit" });
+      fireEvent.click(submitButton);
+
+      const successMessage = screen.queryByText(
+        "User test name successfully registered!!"
+      );
+
+      expect(successMessage).toBeVisible();
+    });
+  });
 });
