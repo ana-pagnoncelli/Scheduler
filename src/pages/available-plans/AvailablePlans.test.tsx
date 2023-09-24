84ed078e7b7bb3f76f9f915096dd37385de4d7ab
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-09-24 20:52:11 -0300

    Create some content in available plans page

--- src/pages/available-plans/AvailablePlans.test.tsx
+++ src/pages/available-plans/AvailablePlans.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { AvailablePlans } from "./AvailablePlans";
 
-test("renders scheduler page", () => {
+test("renders available plans page", () => {
   render(<AvailablePlans />);
-  const linkElement = screen.getByText("This is the available plans page");
+  const linkElement = screen.getByText("Available plans");
   expect(linkElement).toBeInTheDocument();
 });
