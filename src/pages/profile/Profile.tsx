0fd9b7fb9efdfa376297b1f4f48d37f1e066ae01
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2023-09-24 23:28:18 -0300

    start creating profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,38 +1,91 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Input, Label } from "./styles";
 
 export function Profile() {
   return (
-    <>
-      <h1>This is the profile page</h1>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
-        tellus. Convallis convallis tellus id interdum velit laoreet id donec
-        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
-        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        Lorem donec massa sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-        ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
-        integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-        lacus sed viverra tellus. Purus sit amet volutpat consequat mauris.
-        Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-        accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac.
-        Pellentesque nec nam aliquam sem et tortor. Habitant morbi tristique
-        senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
-        maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-        aliquam ultrices sagittis orci a.
-      </Typography>
-    </>
+    <div>
+      <h1>Profile Page</h1>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <Grid item xs={4}>
+            <Label className='label' htmlFor='email'>
+              Email
+              <Input
+                // onChange={handleEmail}
+                className='input'
+                // value={email}
+                type='email'
+                id='email'
+              />
+            </Label>
+          </Grid>
+          <Grid item xs={4}>
+            <Label className='label' htmlFor='email'>
+              Email
+              <Input
+                // onChange={handleEmail}
+                className='input'
+                // value={email}
+                type='email'
+                id='email'
+              />
+            </Label>
+          </Grid>
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <Grid item xs={4}>
+            <Label className='label' htmlFor='email'>
+              Email
+              <Input
+                // onChange={handleEmail}
+                className='input'
+                // value={email}
+                type='email'
+                id='email'
+              />
+            </Label>
+          </Grid>
+          <Grid item xs={4}>
+            <Label className='label' htmlFor='email'>
+              Email
+              <Input
+                // onChange={handleEmail}
+                className='input'
+                // value={email}
+                type='email'
+                id='email'
+              />
+            </Label>
+          </Grid>
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <Grid item xs={4}>
+            <Label className='label' htmlFor='email'>
+              Email
+              <Input
+                // onChange={handleEmail}
+                className='input'
+                // value={email}
+                type='email'
+                id='email'
+              />
+            </Label>
+          </Grid>
+          <Grid item xs={4}>
+            <Label className='label' htmlFor='email'>
+              Email
+              <Input
+                // onChange={handleEmail}
+                className='input'
+                // value={email}
+                type='email'
+                id='email'
+              />
+            </Label>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
