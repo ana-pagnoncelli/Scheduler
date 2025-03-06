a41580904c5a093c8111b85267b3004a7880bb5e
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2025-03-06 03:21:00 -0300

    my schedule starting...

---
+++ src/pages/home/styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+import { BaseApp, BaseBox } from "../styles";
+
+export const HomeBox = styled(BaseBox)({
+  maxWidth: "1000px"
+});
+
+export const HomeApp = styled(BaseApp)({
+  minHeight: "100vh"
+});
---
+++ src/pages/schedule/MySchedule/MySchedule.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import RestoreIcon from "@mui/icons-material/Restore";
+import { ScheduleBox } from "../styles";
+
+export function MySchedule() {
+  return (
+    <ScheduleBox>
+      <Typography variant='h3'>My schedule</Typography>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <AutoAwesomeIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary='Date and time of next class'
+            secondary='Jan 9, 2014'
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <CalendarMonthIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary='Fixed schedules' secondary='Jan 7, 2014' />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <RestoreIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary='Number of classes to recover'
+            secondary='July 20, 2014'
+          />
+        </ListItem>
+      </List>
+    </ScheduleBox>
+  );
+}
